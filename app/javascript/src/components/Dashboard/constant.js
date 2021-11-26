b95a17e0ac20e4acd035a3e3268801e6ed20da1d
nehamaria <[nehamariasanil@gmail.com]>
2021-11-26 23:02:01 +0530

    Created SubHeader

---
+++ app/javascript/src/components/Dashboard/constant.js
@@ -0,0 +1,22 @@
+export const COLUMNS = [
+  {
+    Header: "TITLE",
+    accessor: "title",
+  },
+  {
+    Header: "DATE",
+    accessor: "created_at",
+  },
+  {
+    Header: "AUTHOR",
+    accessor: "author_name",
+  },
+  {
+    Header: "CATEGORY",
+    accessor: "category.name",
+  },
+  {
+    Header: "STATUS",
+    accessor: "status",
+  },
+];

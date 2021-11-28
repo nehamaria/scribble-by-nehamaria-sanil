d4b4598114959df9b489b64dbce75039d4726f73
nehamaria <[nehamariasanil@gmail.com]>
2021-11-28 20:39:26 +0530

    Updated submit button in form

--- app/javascript/src/components/Dashboard/Article/Form/Submit.jsx
+++ app/javascript/src/components/Dashboard/Article/Form/Submit.jsx
@@ -5,25 +5,22 @@ import { Button, Dropdown } from "neetoui";
 
 const Submit = ({ isSubmitting, setStatus, status }) => {
   return (
-    <div className="flex rounded-md overflow-hidden neeto-ui-bg-secondary-indigo">
-      <Button
-        type="submit"
-        label={status}
-        size="large"
-        className=" rounded-l-none "
-        disabled={isSubmitting}
-        loading={isSubmitting}
-      />
-      <Dropdown
-        icon={() => <Down className="border-l border-white  " />}
-        position="bottom-end"
-        buttonProps={{
-          className: " rounded-r-none ",
-        }}
-      >
-        <li onClick={() => setStatus("Save Draft")}>Save Draft</li>
-        <li onClick={() => setStatus("Save Published")}>Save Published</li>
-      </Dropdown>
+    <div className="flex rounded-md overflow-hidden bg-indigo-100 border-2 border-indigo-600">
+      <div className="overflow-hidden mr-px neeto-ui-bg-secondary-indigo ">
+        <Button
+          type="submit"
+          label={status}
+          size="large"
+          disabled={isSubmitting}
+          loading={isSubmitting}
+        />
+      </div>
+      <div className="neeto-ui-bg-secondary-indigo">
+        <Dropdown icon={() => <Down className="" />} position="bottom-end">
+          <li onClick={() => setStatus("Save Draft")}>Save Draft</li>
+          <li onClick={() => setStatus("Save Published")}>Save Published</li>
+        </Dropdown>
+      </div>
     </div>
   );
 };

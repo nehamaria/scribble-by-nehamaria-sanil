559f8c7bdc45152d0ce2e418ba27744cb94ecddc
nehamaria <[nehamariasanil@gmail.com]>
2021-12-05 20:29:08 +0530

    Updated Articles Form

--- app/javascript/src/components/Dashboard/Article/Edit/index.jsx
+++ app/javascript/src/components/Dashboard/Article/Edit/index.jsx
@@ -16,7 +16,6 @@ const Edit = () => {
   const history = useHistory();
   const { id } = useParams();
   const [loading, setLoading] = useState(true);
-  const [submitted, setSubmitted] = useState(false);
 
   const setFormValues = articles => {
     const selectedForm = (({ title, body, category }) => ({
@@ -35,7 +34,6 @@ const Edit = () => {
 
   const fetchDetails = async () => {
     try {
-      setSubmitted(true);
       const article = await articleApi.show(id);
       setArticleDetails(article.data.articles);
       const response = await categoryApi.categoryList();
@@ -53,7 +51,7 @@ const Edit = () => {
     }
   };
 
-  const handleSubmit = async values => {
+  const handleSubmit = async (values, { setSubmitting }) => {
     const { title, category, body } = values;
     const payload = {
       title: title,
@@ -65,6 +63,7 @@ const Edit = () => {
       await articleApi.update(id, payload);
       history.push("/");
     } catch (error) {
+      setSubmitting(false);
       logger.error(error);
     }
   };
@@ -82,8 +81,6 @@ const Edit = () => {
       categoryList={categoryList}
       handleSubmit={handleSubmit}
       initialForm={initialForm}
-      submitted={submitted}
-      setSubmitted={setSubmitted}
     />
   );
 };

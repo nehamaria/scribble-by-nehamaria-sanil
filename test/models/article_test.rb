1f6712314ac6f8f0c3fa630c42b5660d5c06dc80
nehamaria <[nehamariasanil@gmail.com]>
2021-12-09 00:19:45 +0530

    Added unit validation tests

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -4,7 +4,9 @@ require "test_helper"
 
 class ArticleTest < ActiveSupport::TestCase
   def setup
-    @article = build(:article)
+    @article = create(:article)
+    @category = create(:category)
+    @user = create(:user)
   end
 
   def test_article_should_be_valid
@@ -32,5 +34,33 @@ class ArticleTest < ActiveSupport::TestCase
   def test_title_should_be_of_valid_length
     @article.title = "a" * 100
     assert @article.invalid?
+  end
+
+  def test_slug_is_parameterized_title
+    title = @article.title
+    @article.save!
+    assert_equal title.parameterize, @article.slug
+  end
+
+  def test_slug_generation_for_articles_having_titles_one_being_prefix_of_the_other
+    article_one = Article.create!(
+      title: "ring", body: "ring", status: "Draft", category_id: @category.id,
+      user_id: @user.id)
+    article_two = Article.create!(
+      title: "ringing", body: "ringing", status: "Published",
+      category_id: @category.id, user_id: @user.id)
+    assert_equal "ring", article_one.slug
+    assert_equal "ringing", article_two.slug
+  end
+
+  def test_error_raised_for_duplicate_slug
+    article_test = Article.create!(
+      title: "ring", body: "ring", status: "Draft", category_id: @category.id,
+      user_id: @user.id)
+    assert_raises ActiveRecord::RecordInvalid do
+      article_test.update!(slug: @article.slug)
+    end
+    error_msg = article_test.errors.full_messages.to_sentence
+    assert_match t("article.slug.immutable"), error_msg
   end
 end
---
+++ test/models/redirection_test.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class RedirectionTest < ActiveSupport::TestCase
+  def setup
+    @redirection = build(:redirection)
+  end
+
+  def test_redirection_should_be_valid
+    assert @redirection.valid?
+  end
+
+  def test_from_path_should_be_present
+    @redirection.from_path = nil
+    assert @redirection.invalid?
+    assert_includes @redirection.errors[:from_path], t("errors.messages.blank")
+  end
+
+  def test_to_path_should_be_present
+    @redirection.to_path = nil
+    assert @redirection.invalid?
+    assert_includes @redirection.errors[:to_path], t("errors.messages.blank")
+  end
+
+  def test_should_not_be_valid_and_saved_if_from_path_not_unique
+    @redirection.save!
+
+    test_redirection = @redirection.dup
+    assert_not test_redirection.valid?
+
+    assert_includes test_redirection.errors.full_messages, "From path has already been taken"
+  end
+end

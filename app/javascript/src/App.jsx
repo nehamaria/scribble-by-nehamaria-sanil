caac23189f3cc2b37c2d362c19e5d1e6bbe31a96
nehamaria <[nehamariasanil@gmail.com]>
2021-12-09 00:53:24 +0530

    Added validations to Redirections

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,6 +1,11 @@
 import React, { useEffect, useState } from "react";
 
-import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import {
+  Route,
+  Switch,
+  Redirect,
+  BrowserRouter as Router,
+} from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 
 import { registerIntercepts, setAuthHeaders } from "apis/axios";
@@ -8,14 +13,31 @@ import { initializeLogger } from "common/logger";
 import Dashboard from "components/Dashboard";
 import Public from "components/Public";
 
+import redirectionApi from "./apis/redirection";
+
 const App = () => {
+  const [redirectionList, setRedirectionList] = useState([]);
   const [loading, setLoading] = useState(true);
+
+  const fetchRedirectionList = async () => {
+    setLoading(true);
+    try {
+      const redirections = await redirectionApi.redirectionList();
+      setRedirectionList(redirections.data.redirections);
+    } catch (error) {
+      logger.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
     registerIntercepts();
     initializeLogger();
     setAuthHeaders(setLoading);
+    fetchRedirectionList();
   }, []);
+
   if (loading) {
     return <h1>Loading...</h1>;
   }
@@ -24,6 +46,14 @@ const App = () => {
     <Router>
       <ToastContainer />
       <Switch>
+        {redirectionList.map((redirection, index) => (
+          <Redirect
+            key={index}
+            exact
+            from={redirection.from_path}
+            to={redirection.to_path}
+          />
+        ))}
         <Route path="/public" component={Public} />
         <Route path="/" component={Dashboard} />
       </Switch>
--- app/models/redirection.rb
+++ app/models/redirection.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
 class Redirection < ApplicationRecord
-  validates :from_path, presence: true
+  validates :from_path, presence: true, uniqueness: true
   validates :to_path, presence: true
+  before_validation :validate_paths
+
+  private
+
+    def validate_paths
+      if self.from_path == self.to_path
+        errors.add(:base, "Paths should not be same")
+      end
+    end
 end

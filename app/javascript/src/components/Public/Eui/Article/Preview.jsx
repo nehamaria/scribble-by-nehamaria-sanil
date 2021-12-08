2a1ecb1541975257ea03f43becd26cfc0b7ad315
nehamaria <[nehamariasanil@gmail.com]>
2021-12-09 01:08:40 +0530

    Updated preview page of EUI

--- app/javascript/src/components/Public/Eui/Article/Preview.jsx
+++ app/javascript/src/components/Public/Eui/Article/Preview.jsx
@@ -5,29 +5,34 @@ import { Typography, Tag } from "neetoui";
 import EmptyState from "components/Common/EmptyState";
 
 const Preview = ({ article }) => {
-  const date = new Date(article.created_at).toDateString().split(" ");
+  const date = article
+    ? new Date(article.created_at).toDateString().split(" ")
+    : [];
 
-  return (
-    <>
-      {article ? (
-        <div className="mt-4 space-y-2">
-          <Typography style="h2">{article.title}</Typography>
-          <div className="flex space-x-3">
-            {article?.categoryName && (
-              <Tag color="blue" label={article.categoryName} style="solid" />
-            )}
-            <Typography
-              style="body2"
-              className="neeto-ui-text-gray-500"
-            >{`${date[2]} ${date[1]}, ${date[3]} `}</Typography>
-          </div>
+  return article ? (
+    <div className="mt-4 space-y-2 p-4 overflow-y-auto">
+      <Typography style="h2">{article.title}</Typography>
+      <div className="flex space-x-3">
+        <Tag
+          color="blue"
+          label={
+            <Typography style="body2" className="font-medium text-blue-700">
+              {article.categoryName}
+            </Typography>
+          }
+          style="solid"
+        />
 
-          <Typography style="body2">{article.body}</Typography>
-        </div>
-      ) : (
-        <EmptyState message="No articles found" />
-      )}
-    </>
+        <Typography
+          style="body2"
+          className="neeto-ui-text-gray-500"
+        >{`${date[2]} ${date[1]}, ${date[3]} `}</Typography>
+      </div>
+
+      <Typography style="body2">{article.body}</Typography>
+    </div>
+  ) : (
+    <EmptyState message="No articles found" />
   );
 };
 
